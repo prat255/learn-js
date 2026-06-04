03dadbe41a17069a2d3af513c4eeb20fdb29b0cb
prat255 <pratyoushsrivastava1@gmail.com>
2026-06-04 07:33:00 +0000

    after L09

---
+++ basic/04_comparison.js
@@ -0,0 +1,36 @@
+// console.log(2>1);
+
+// console.log(null> 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// undefined me sabhi ke saath 0 dega
+
+// strict check (===) value with dtype
+// console.log("2"===2)
+// console.log(2===2)
+
+// --------------------------------------
+
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+// non primitive - refrence
+
+// array, objects, functions
+
+let myObj = {
+    name: "Prat",
+    age: 21
+}
+
+const myFunc = function () {
+    console.log("Hello World!!");
+}
+
+// const bigNum = 999999999999999999999999999999999999999999999999999999999999999999999
+// console.log(typeof bigNum);
+console.log(typeof null);
+
+// myFunc()
