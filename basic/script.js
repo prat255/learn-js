1b2cd0147f179f818ab9680725881c06373b11ca
prat255 <pratyoushsrivastava1@gmail.com>
2026-06-04 06:39:25 +0000

    init commit

---
+++ basic/script.js
@@ -0,0 +1,31 @@
+// console.log("prat")
+const accID = 14564
+let accMail = "prat@abc.com"
+var accPswd = "14567"
+accCity = "Gonda"
+let accState;
+
+accMail = "prat222@abc.com"
+accPswd = "145674332"
+accCity = "Gonda086"
+
+// console.table([accID, accMail, accPswd, accCity, accState])
+
+// don't use var due to scope issue
+
+// alert(3+3) //gives alert only in browser, not node
+
+// --------------------------------------
+// data type
+let name = "part"
+let age = 80
+let isloggedin = false
+// null --> standalone value
+// undefined --> var decleared but nothing assigned
+// symbol --> unique
+
+// object
+
+// console.log(typeof 'hiii') //string
+// console.log(typeof null) //object
+
