e8ac981a46b8f67880e0b267784a867404fac6f1
prat255 <pratyoushsrivastava1@gmail.com>
2026-06-04 07:09:27 +0000

    after L07

--- basic/03_conversion.js
+++ basic/03_conversion.js
@@ -1,16 +1,30 @@
 let score = 33
-console.log(typeof score)
+// console.log(typeof score)
 
 score = "33"
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 // "33" -> 33
 // "33abc" -> NaN
 // true -> 1
 
 let isLoggedIn = 5
 let boolIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolIsLoggedIn)
+// console.log(boolIsLoggedIn)
+
+// *******************************************************
+
+let value = 3
+let negValue = -value
+console.log(typeof negValue)
+console.log(typeof value)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1" + 2+ 2)
+console.log(1+2+"2")
+
+
